90920fbe2ca5fb47ed239fbfd0d4a9a3c90bc89d
sswum <l5450won@naver.com>
2024-04-16 17:57:19 +0900

    공통 컴포넌트 - 메인 제목, 버튼, 입력 항목, 회원 가입 페이지 구성 중

--- src/member/pages/Join.js
+++ src/member/pages/Join.js
@@ -3,23 +3,22 @@ import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import JoinForm from '../components/JoinForm';
 import { OuterBox } from '../../commons/components/LayoutBox';
+import { MainTitle } from '../../commons/components/TitleBox';
 
 const Join = () => {
   const { t } = useTranslation();
 
-  return(
-<>
-<Helmet>
-<title> {t('회원가입')}</title>
-</Helmet>
+  return (
+    <>
+      <Helmet>
+        <title> {t('회원가입')}</title>
+      </Helmet>
 
-<OuterBox>
-<h1>{t('회원가입')}</h1>
-<JoinForm />
-</OuterBox>
-</>
-
-
+      <OuterBox>
+        <MainTitle>{t('회원가입')}</MainTitle>
+        <JoinForm />
+      </OuterBox>
+    </>
   );
 };
 
